a7e3772225b53a8662c4463f4530386572e30aae
Johann Zelger <zelger@me.com>
2014-03-17 20:19:16 +0100

    fixed last error handling after php process finished

--- src/TechDivision/WebServer/Modules/PhpModule.php
+++ src/TechDivision/WebServer/Modules/PhpModule.php
@@ -196,10 +196,13 @@ class PhpModule implements ModuleInterface
             // check if process fatal error occurred so throw module exception because the modules process class
             // is not responsible for set correct headers and messages for error's in module context
             if ($lastError = $process->getLastError()) {
-                $errorMessage = 'PHP Fatal error: ' . $lastError['message'] .
-                    ' in ' . $lastError['file'] . ' on line ' . $lastError['line'];
-                // set internal server error code with error mesage to exception
-                throw new ModuleException($errorMessage, 500);
+                // check if last error was a fatal one
+                if ($lastError['type'] === 1) {
+                    $errorMessage = 'PHP Fatal error: ' . $lastError['message'] .
+                        ' in ' . $lastError['file'] . ' on line ' . $lastError['line'];
+                    // set internal server error code with error mesage to exception
+                    throw new ModuleException($errorMessage, 500);
+                }
             }
 
             // prepare response

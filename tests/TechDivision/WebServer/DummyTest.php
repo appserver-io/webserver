59b99d8c78d239a46ca9020ca7538c5ef4a60a31
wick-ed <b.wick@techdivision.com>
2014-06-12 18:43:10 +0200

    phpcs

--- tests/TechDivision/WebServer/DummyTest.php
+++ tests/TechDivision/WebServer/DummyTest.php
@@ -12,7 +12,7 @@
  *
  * @category   Webserver
  * @package    TechDivision_WebServer
- * @subpackage tests
+ * @subpackage Tests
  * @author     Johann Zelger <jz@techdivision.com>
  * @copyright  2014 TechDivision GmbH <info@techdivision.com>
  * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
@@ -21,25 +21,26 @@
 
 namespace TechDivision\WebServer;
 
-use TechDivision\WebServer\Dictionaries\ServerVars;
-
 /**
  * Class DummyTest
  *
  * @category   Webserver
  * @package    TechDivision_WebServer
- * @subpackage tests
+ * @subpackage Tests
  * @author     Johann Zelger <jz@techdivision.com>
  * @copyright  2014 TechDivision GmbH <info@techdivision.com>
  * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  * @link       https://github.com/techdivision/TechDivision_Http
  */
-class DummyTest extends \PHPUnit_Framework_TestCase {
-
+class DummyTest extends \PHPUnit_Framework_TestCase
+{
     /**
      * Test set server var functionality on response object.
+     *
+     * @return void
      */
-    public function testDummy() {
-        $this->assertSame(1,1);
+    public function testDummy()
+    {
+        $this->assertSame(1, 1);
     }
 }

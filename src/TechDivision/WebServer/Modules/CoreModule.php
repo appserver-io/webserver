62dd0dd608967902e5b7d3876f3c58e4aa27e6ac
Johann Zelger <zelger@me.com>
2014-11-03 12:10:46 +0100

    separeted request context expansion

--- src/TechDivision/WebServer/Modules/CoreModule.php
+++ src/TechDivision/WebServer/Modules/CoreModule.php
@@ -71,6 +71,129 @@ class CoreModule implements ModuleInterface
     protected $locations;
 
     /**
+     * Expands request context on given request constellation (uri) based on file handler configuration
+     *
+     * @param
+     *
+     * @return void
+     */
+    public function expandRequestContext(RequestContextInterface $requestContext) {
+        // get local refs
+        $serverContext = $this->getServerContext();
+
+        // get document root
+        $documentRoot = $requestContext->getServerVar(ServerVars::DOCUMENT_ROOT);
+        // get handlers
+        $handlers = $serverContext->getServerConfig()->getHandlers();
+
+        // get uri without querystring
+        // Just make sure that you check for the existence of the query string first, as it might not be set
+        $uriWithoutQueryString = parse_url($requestContext->getServerVar(ServerVars::X_REQUEST_URI), PHP_URL_PATH);
+
+        // split all path parts got from uri without query string
+        $pathParts = explode('/', $uriWithoutQueryString);
+
+        // init vars for path parsing
+        $possibleValidPathExtension = '';
+        $possibleValidPath = '';
+        $pathInfo = '';
+        $validDir = null;
+        $scriptName = null;
+        $scriptFilename = null;
+
+        // note: only if file extension hits a filehandle info it will be possible to set path info etc...
+
+        // iterate through all dirs beginning at 1 because 0 is always empty in this case
+        for ($i = 1; $i < count($pathParts); ++$i) {
+            // check if no script name was found yet
+            if (!$scriptName) {
+                // append valid path
+                $possibleValidPath .= DIRECTORY_SEPARATOR . $pathParts[$i];
+                // get possible extension
+                $possibleValidPathExtension = pathinfo($possibleValidPath, PATHINFO_EXTENSION);
+                // check if dir does not exists
+                if (!is_dir($documentRoot . $possibleValidPath)) {
+                    // check if its not a existing file
+                    if (!is_file($documentRoot . $possibleValidPath)) {
+                        // check if file handler is defined for that virtual file
+                        if (isset($handlers['.' . $possibleValidPathExtension])) {
+                            // set script name for further processing as script aspect
+                            $scriptName = $possibleValidPath;
+                        }
+                    } else {
+                        // set script name
+                        $scriptName = $possibleValidPath;
+                        // set script filename
+                        $scriptFilename = $documentRoot . $scriptName;
+                    }
+                } else {
+                    // save valid dir for indexed surfing later on
+                    $validDir = $possibleValidPath;
+                }
+            } else {
+                // else build up path info
+                $pathInfo .= DIRECTORY_SEPARATOR . $pathParts[$i];
+            }
+        }
+
+        // load the locations
+        $locations = $this->locations;
+
+        // check if there are some volatile location definitions so use them and override global locations
+        if ($requestContext->hasModuleVar(ModuleVars::VOLATILE_LOCATIONS)) {
+            $locations = $requestContext->getModuleVar(ModuleVars::VOLATILE_LOCATIONS);
+        }
+
+        // process the locations for this request
+        foreach ($locations as $location) {
+            if (preg_match('/' . $location['condition'] . '/', $uriWithoutQueryString)) {
+                // if we find a configured file handler responsible for the path extension of this request
+                if (isset($location['handlers']['.' . $possibleValidPathExtension])) {
+                    // set/overwrite the default handler
+                    $handlers['.' . $possibleValidPathExtension] = $location['handlers']['.' . $possibleValidPathExtension];
+                    break;
+                }
+            }
+        }
+
+        // set special server var for requested file
+        $requestContext->setServerVar(ServerVars::REQUEST_FILENAME, $documentRoot . $possibleValidPath);
+
+        // check if requested file is on filesystem and set it to be script filename
+        if ($scriptFilename) {
+            $requestContext->setServerVar(ServerVars::SCRIPT_FILENAME, $scriptFilename);
+        }
+
+        // check if file handler is defined for that script and expand request context
+        if (isset($handlers['.' . $possibleValidPathExtension])) {
+
+            // set specific server vars
+            $requestContext->setServerVar(ServerVars::SCRIPT_NAME, $scriptName);
+
+            // if path info is set put it into server vars
+            if (strlen($pathInfo) > 0) {
+                // set path info vars
+                $requestContext->setServerVar(ServerVars::PATH_INFO, $pathInfo);
+                $requestContext->setServerVar(ServerVars::PATH_TRANSLATED, $documentRoot . $pathInfo);
+            }
+
+            // set the file handler to use for modules being able to react on this setting
+            $requestContext->setServerVar(
+                ServerVars::SERVER_HANDLER,
+                $handlers['.' . $possibleValidPathExtension]['name']
+            );
+
+            // if file handler params are given, set them as module var
+            if (isset($handlers['.' . $possibleValidPathExtension]['params'])) {
+                $requestContext->setModuleVar(
+                    ModuleVars::VOLATILE_FILE_HANDLER_VARIABLES,
+                    $handlers['.' . $possibleValidPathExtension]['params']
+                );
+            }
+        }
+    }
+
+    /**
      * Implement's module logic for given hook
      *
      * @param \TechDivision\Connection\ConnectionRequestInterface     $request        A request object
@@ -82,179 +205,86 @@ class CoreModule implements ModuleInterface
      * @throws \TechDivision\Server\Exceptions\ModuleException
      */
     public function process(
+        // In php an interface is, by definition, a fixed contract. It is immutable.
+        // So we have to declare the right ones afterwards...
+        /** @var $request \TechDivision\Http\HttpRequestInterface */
         ConnectionRequestInterface $request,
+        /** @var $response \TechDivision\Http\HttpResponseInterface */
         ConnectionResponseInterface $response,
         RequestContextInterface $requestContext,
         $hook
     ) {
-        // In php an interface is, by definition, a fixed contract. It is immutable.
-        // So we have to declair the right ones afterwards...
-        /** @var $request \TechDivision\Http\HttpRequestInterface */
-        /** @var $response \TechDivision\Http\HttpResponseInterface */
 
         // if false hook is comming do nothing
         if (ModuleHooks::REQUEST_POST !== $hook) {
             return;
         }
 
-        // set local vars
-        $serverContext = $this->getServerContext();
-
         // check if core module should still handle this request
         // maybe later on this can be overwritten by another core module for some reasons
         if ($requestContext->getServerVar(ServerVars::SERVER_HANDLER) === self::MODULE_NAME) {
-            // get document root
-            $documentRoot = $requestContext->getServerVar(ServerVars::DOCUMENT_ROOT);
-            // get handlers
-            $handlers = $serverContext->getServerConfig()->getHandlers();
 
-            // get uri without querystring
-            // Just make sure that you check for the existence of the query string first, as it might not be set
-            $uriWithoutQueryString = parse_url($requestContext->getServerVar(ServerVars::X_REQUEST_URI), PHP_URL_PATH);
+            // expand request context for possible script calling based on file handler configurations
+            $this->expandRequestContext($requestContext);
 
-            // split all path parts got from uri without query string
-            $pathParts = explode('/', $uriWithoutQueryString);
-            // init vars for path parsing
-            $possibleValidPath = '';
-            $pathInfo = '';
-            $validDir = null;
-            $scriptName = null;
-            $scriptFilename = null;
+            // check if file handler is still core module
+            if ($requestContext->getServerVar(ServerVars::SERVER_HANDLER) === self::MODULE_NAME) {
 
-            // note: only if file extension hits a filehandle info it will be possible to set path info etc...
+                // get document root
+                $documentRoot = $requestContext->getServerVar(ServerVars::DOCUMENT_ROOT);
 
-            // iterate through all dirs beginning at 1 because 0 is always empty in this case
-            for ($i = 1; $i < count($pathParts); ++$i) {
-                // check if no script name was found yet
-                if (!$scriptName) {
-                    // append valid path
-                    $possibleValidPath .= DIRECTORY_SEPARATOR . $pathParts[$i];
-                    // get possible extension
-                    $possibleValidPathExtension = pathinfo($possibleValidPath, PATHINFO_EXTENSION);
-                    // check if dir does not exists
-                    if (!is_dir($documentRoot . $possibleValidPath)) {
-                        // check if its a existing file
-                        if (!is_file($documentRoot . $possibleValidPath)) {
-                            // check if file handler is defined for that virtual file
-                            if (isset($handlers['.' . $possibleValidPathExtension])) {
-                                // set script name for further processing as script aspect
-                                $scriptName = $possibleValidPath;
-                            }
-                        } else {
-                            // set script name
-                            $scriptName = $possibleValidPath;
-                            // set script filename
-                            $scriptFilename = $documentRoot . $scriptName;
-                        }
-                    } else {
-                        // save valid dir for indexed surfing later on
-                        $validDir = $possibleValidPath;
-                    }
-                } else {
-                    // else build up path info
-                    $pathInfo .= DIRECTORY_SEPARATOR . $pathParts[$i];
-                }
-            }
+                // if existing file should be served
+                if ($requestContext->hasServerVar(ServerVars::SCRIPT_FILENAME)) {
 
-            // load the locations
-            $locations = $this->locations;
+                    $scriptFilename = $requestContext->getServerVar(ServerVars::SCRIPT_FILENAME);
 
-            // check if there are some volatile location definitions so use them and override global locations
-            if ($requestContext->hasModuleVar(ModuleVars::VOLATILE_LOCATIONS)) {
-                $locations = $requestContext->getModuleVar(ModuleVars::VOLATILE_LOCATIONS);
-            }
+                    // get file info
+                    $fileInfo = new \SplFileInfo($scriptFilename);
 
-            // process the locations for this request
-            foreach ($locations as $location) {
-                if (preg_match('/' . $location['condition'] . '/', $uriWithoutQueryString)) {
-                    // if we find a configured file handler responsible for the path extension of this request
-                    if (isset($location['handlers']['.' . $possibleValidPathExtension])) {
-                        // set/overwrite the default handler
-                        $handlers['.' . $possibleValidPathExtension] = $location['handlers']['.' . $possibleValidPathExtension];
-                        break;
-                    }
-                }
-            }
+                    // build etag
+                    $eTag = sprintf('"%x-%x-%x"', $fileInfo->getInode(), $fileInfo->getSize(), (float)str_pad($fileInfo->getMTime(), 16, '0'));
 
-            // check if file handler is defined for that script
-            if (isset($handlers['.' . $possibleValidPathExtension])) {
+                    // set last modified header
+                    $response->addHeader(HttpProtocol::HEADER_LAST_MODIFIED, gmdate(DATE_RFC822, $fileInfo->getMTime()));
 
-                // set specific server vars
-                $requestContext->setServerVar(ServerVars::SCRIPT_NAME, $scriptName);
-                // check if script is on filesystem
-                if ($scriptFilename) {
-                    $requestContext->setServerVar(ServerVars::SCRIPT_FILENAME, $scriptFilename);
-                    // set special server var for existing file for that request
-                    $requestContext->setServerVar(ServerVars::REQUEST_FILENAME, $scriptFilename);
-                }
-                // if path info is set put it into server vars
-                if (strlen($pathInfo) > 0) {
-                    // set path info vars
-                    $requestContext->setServerVar(ServerVars::PATH_INFO, $pathInfo);
-                    $requestContext->setServerVar(ServerVars::PATH_TRANSLATED, $documentRoot . $pathInfo);
-                }
+                    // set etag header
+                    $response->addHeader(HttpProtocol::HEADER_ETAG, $eTag);
 
-                // set the file handler to use for modules being able to react on this setting
-                $requestContext->setServerVar(
-                    ServerVars::SERVER_HANDLER,
-                    $handlers['.' . $possibleValidPathExtension]['name']
-                );
-
-                // if file handler params are given, set them as module var
-                if (isset($handlers['.' . $possibleValidPathExtension]['params'])) {
-                    $requestContext->setModuleVar(
-                        ModuleVars::VOLATILE_FILE_HANDLER_VARIABLES,
-                        $handlers['.' . $possibleValidPathExtension]['params']
+                    // set correct mimetype header
+                    $response->addHeader(
+                        HttpProtocol::HEADER_CONTENT_TYPE,
+                        MimeTypes::getMimeTypeByExtension($fileInfo->getExtension())
                     );
+
+                    // caching checks
+                    if (($request->hasHeader(HttpProtocol::HEADER_IF_NONE_MATCH)) &&
+                        ($request->getHeader(HttpProtocol::HEADER_IF_NONE_MATCH) === $eTag)
+                    ) {
+                        // set not modified status without content
+                        $response->setStatusCode(304);
+                    } else {
+                        // serve file by set body stream to file descriptor stream
+                        $response->setBodyStream(fopen($scriptFilename, "r"));
+                    }
+
+                    // set response state to be dispatched after this without calling other modules process
+                    $response->setState(HttpResponseStates::DISPATCH);
+
+                    // go out
+                    return;
                 }
 
-                // go out
-                return;
+                // if we got here its maybe a directory index surfing request if $validDir is same as uri
+                // todo: implement directory index view and surfing
 
-            // else check if file exists and we got a
-            } elseif ($scriptFilename) {
-
-                // get file info
-                $fileInfo = new \SplFileInfo($scriptFilename);
-
-                // build etag
-                $eTag = sprintf('"%x-%x-%x"', $fileInfo->getInode(), $fileInfo->getSize(), (float)str_pad($fileInfo->getMTime(), 16, '0'));
-
-                // set last modified header
-                $response->addHeader(HttpProtocol::HEADER_LAST_MODIFIED, gmdate(DATE_RFC822, $fileInfo->getMTime()));
-
-                // set etag header
-                $response->addHeader(HttpProtocol::HEADER_ETAG, $eTag);
-
-                // set correct mimetype header
-                $response->addHeader(
-                    HttpProtocol::HEADER_CONTENT_TYPE,
-                    MimeTypes::getMimeTypeByExtension($possibleValidPathExtension)
-                );
-
-                // caching checks
-                if (($request->hasHeader(HttpProtocol::HEADER_IF_NONE_MATCH)) &&
-                    ($request->getHeader(HttpProtocol::HEADER_IF_NONE_MATCH) === $eTag)) {
-                    // set not modified status without content
-                    $response->setStatusCode(304);
-                } else {
-                    // serve file by set body stream to file descriptor stream
-                    $response->setBodyStream(fopen($scriptFilename, "r"));
-                }
-
-                // set response state to be dispatched after this without calling other modules process
-                $response->setState(HttpResponseStates::DISPATCH);
-
-                // go out
-                return;
-
+            } else {
+                // for now we will throw a 404 as well here for non existing index files in directory
+                throw new ModuleException(
+                    sprintf(
+                        "Requested directory or filename '%s' not found.",
+                        $requestContext->getServerVar(ServerVars::REQUEST_FILENAME)
+                    ), 404);
             }
-
-            // if we got here its maybe a directory index surfing request if $validDir is same as uri
-            // todo: implement directory index view and surfing
-
-            // for now we will throw a 404 as well here for non existing index files in directory
-            throw new ModuleException("Requested directory or filename '$documentRoot$possibleValidPath' not found.", 404);
         }
     }
 
